4acee83fe9431ed31ab338837105b33be55a9ef5
root <root@ip-AC101104.gvjlen3bax4efc1hl40cnjtwzg.ax.internal.cloudapp.net>
2022-04-12 12:50:18 +0000

    first commit

---
+++ iotaptest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp .bashrc  testerwrite
+cp /shared/software/stap/kernel-debuginfo-common-x86_64-3.10.0-1127.19.1.el7.x86_64.rpm /tmp
+
+rm /tmp/kernel-debuginfo-common-x86_64-3.10.0-1127.19.1.el7.x86_64.rpm
